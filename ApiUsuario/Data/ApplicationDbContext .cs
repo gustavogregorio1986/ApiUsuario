fe54e8d854075b1e4a9cfcd47e2f572cdf41fbb6
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-07-07 12:15:04 -0300

    Add project files.

---
+++ ApiUsuario/Data/ApplicationDbContext .cs
@@ -0,0 +1,15 @@
+﻿using ApiUsuario.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiUsuario.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<UsuarioModel> Usuarios { get; set; }
+    }
+}
